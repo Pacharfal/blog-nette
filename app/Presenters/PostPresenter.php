f29f905fec21310fe9a95c2111b5393d619e1b38
Jan Kouřil <xkouril1@mendelu.cz>
2023-02-09 17:42:40 +0100

    add comments

--- app/Presenters/PostPresenter.php
+++ app/Presenters/PostPresenter.php
@@ -8,14 +8,51 @@ final class PostPresenter extends Nette\Application\UI\Presenter
 {
     public function __construct(
         private Nette\Database\Explorer $database,
-	) {
+    )
+    {
     }
 
     public function renderShow(int $postId): void
     {
-        $this->template->post = $this->database
+        $post = $this->database
             ->table('posts')
             ->get($postId);
+        if (!$post) {
+            $this->error('Stránka nebyla nalezena');
+        }
+
+        $this->template->post = $post;
+
+    }
+    protected function createComponentCommentForm(): Form
+    {
+        $form = new Form; // means Nette\Application\UI\Form
+
+        $form->addText('name', 'Jméno:')
+            ->setRequired();
+
+        $form->addEmail('email', 'E-mail:');
+
+        $form->addTextArea('content', 'Komentář:')
+            ->setRequired();
+
+        $form->addSubmit('send', 'Publikovat komentář');
+        $form->onSuccess[] = [$this, 'commentFormSucceeded'];
+        return $form;
+    }
+    public function commentFormSucceeded(\stdClass $data): void
+    {
+        $postId = $this->getParameter('postId');
+
+        $this->database->table('comments')->insert([
+            'post_id' => $postId,
+            'name' => $data->name,
+            'email' => $data->email,
+            'content' => $data->content,
+        ]);
+
+        $this->flashMessage('Děkuji za komentář', 'success');
+        $this->redirect('this');
     }
 }
 
