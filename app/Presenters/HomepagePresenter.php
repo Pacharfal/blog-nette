fca276c449d5aa89a6edc5de2f3bc62721e2f5f6
Jan Kouřil <xkouril1@mendelu.cz>
2023-02-10 15:20:56 +0100

    you can eddit posts now + model layer added

--- app/Presenters/HomepagePresenter.php
+++ app/Presenters/HomepagePresenter.php
@@ -4,25 +4,21 @@ declare(strict_types=1);
 
 namespace App\Presenters;
 
+use App\Model\PostFacade;
 use Nette;
-use Nette\Application\UI\Form;
 
 final class HomepagePresenter extends Nette\Application\UI\Presenter
 {
-    private Nette\Database\Explorer $database;
-
-    public function __construct(Nette\Database\Explorer $database)
-    {
-        $this->database = $database;
+    public function __construct(
+        private PostFacade $facade,
+    ) {
     }
+
     public function renderDefault(): void
     {
-        $this->template->posts = $this->database
-            ->table('posts')
-            ->order('created_at DESC')
+        $this->template->posts = $this->facade
+            ->getPublicArticles()
             ->limit(5);
     }
-
-
 }
 
